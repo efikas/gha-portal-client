5753c17fb3cdce675ed6fef28ff6d1addac68d8b
Fashola Ayodeji <fashtop3@gmail.com>
2018-07-18 09:59:52 +0100

    fixing student profile

---
+++ src/components/pages/student/partials/profile/academic.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "academic"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/pages/student/partials/profile/guardian.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "guardian"
+    }
+</script>
+
+<style scoped>
+
+</style>
